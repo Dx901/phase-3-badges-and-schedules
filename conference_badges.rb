bc8f0282d88f7480136658cffaa2851ea8839c2d
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-24 08:27:08 +0300

    DONE

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendance)
+    array=[]
+    attendance.each {|name|array<<badge_maker(name)}
+    array
+end
+
+def assign_rooms(attendance)
+    array = []
+    attendance.each_with_index do |name, index|
+        array << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    array
+
+end
+
+def printer(attendance)
+    batch_badge_creator(attendance).each{|names| puts names}
+    assign_rooms(attendance).each { |room|puts room}
+end
